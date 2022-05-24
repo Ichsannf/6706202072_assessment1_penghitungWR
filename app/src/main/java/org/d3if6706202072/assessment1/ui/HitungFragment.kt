b25b824d968fb20414442146bf052ffdbbcf429f
muhammad_ichsan_nurfauzi <ichsanfauzi354@gmail.com>
2022-05-24 12:56:48 +0700

    memenuhi point 4 sampai 6 di assessment2

--- app/src/main/java/org/d3if6706202072/assessment1/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if6706202072/assessment1/ui/HitungFragment.kt
@@ -1,9 +1,8 @@
 package org.d3if6706202072.assessment1.ui
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -23,6 +22,7 @@ class HitungFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         binding = FragmentHitungBinding.inflate(layoutInflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -33,7 +33,21 @@ class HitungFragment : Fragment() {
             it.findNavController().navigate(R.id.action_hitungFragment_to_saranFragment)
         }
         viewModel.getHasilWr().observe(requireActivity(), { showResult(it) })
+        binding.shareButton.setOnClickListener { shareData() }
 
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(
+                R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
@@ -82,15 +96,19 @@ class HitungFragment : Fragment() {
         if (result == null) return
         binding.hasilId.text = getString(R.string.hasil, result.wr.toString())
         binding.saranButton.visibility = View.VISIBLE
+        binding.buttonGroup.visibility = View.VISIBLE
     }
-
-    private fun hitungWR(tMatch: Float, tWr: Float, wrReq: Float): HasilWr {
-        val tWin = tMatch * (tWr / 100)
-        val tLose = tMatch - tWin
-        val sisaWr = 100 - wrReq
-        val wrResult = 100 / sisaWr
-        val seratusPersen = tLose * wrResult
-        val final = (seratusPersen - tMatch)
-        return HasilWr(final)
+    private fun shareData() {
+        val message = getString(R.string.bagikan_template,
+            binding.numatch.text,
+            binding.nutowr.text,
+            binding.nuwr.text,
+        )
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, message)
+        if (shareIntent.resolveActivity(
+                requireActivity().packageManager) != null) {
+            startActivity(shareIntent)
+        }
     }
 }
